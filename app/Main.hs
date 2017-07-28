54f2c4e50d23061455d2254cdc944250b9eb1fd5
Levi Soto <levisoto.337@gmail.com>
2017-07-28 18:47:08 -0500

    Adding tests and some functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import Lib
+import LogAnalysis
+import Log
 
-main :: IO ()
-main = someFunc
+main :: IO [LogMessage]
+main = testParse parse 10 "data/error.log"
+--testParse parse 10 "error.log"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,21 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import LogAnalysis
+import Log
+
+logAnalysisSuite :: TestTree
+logAnalysisSuite = testGroup "Test of Function ParseMessage"
+                  [testCase "test 1" $ parseMessage "E 2 562 help help" @?= LogMessage (Error 2) 562 "help help"
+                  ,testCase "test 2" $ parseMessage "I 29 la la la" @?= LogMessage Info 29 "la la la"
+                  ]
+
+main = defaultMain logAnalysisSuite
+
+
+--main :: IO ()
+--main = putStrLn "Hola me llamo levi Y voy a ser el mejor"
+
+
